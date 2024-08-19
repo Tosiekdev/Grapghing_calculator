9f48ce3ee9ff55485bb86abda0a98ab72f514d34
Tosiekdev <antonizieciak@gmail.com>
2024-08-19 21:50:30 +0200

    A little bit more const in canvas.cpp

--- Application/canvas.cpp
+++ Application/canvas.cpp
@@ -23,32 +23,32 @@ void Canvas::draw(sf::RenderWindow& window) {
 }
 
 void Canvas::set_lines(sf::RenderWindow& window) {
-    float x = static_cast<float>(window.getSize().x);
-    float y = static_cast<float>(window.getSize().y);
+    const float x = static_cast<float>(window.getSize().x);
+    const float y = static_cast<float>(window.getSize().y);
 
     // width of the coordinate system
-    float graphWidth = x - x / 3.f;
+    const float graphWidth = x - x / 3.f;
 
     // y position of lines and x position of starting vertical line
-    float verticalLineY = y / 13.5f;
+    const float verticalLineY = y / 13.5f;
     float step = graphWidth / 12.f;
     float whole = std::abs(_startEndHorizontal.second - std::floor(_startEndHorizontal.second));
     float verticalStartX = x / 3.f + (1 - whole) * step;
 
     // x position of lines and y position of starting horizontal line
-    float horizontalLineX = x / 3.f;
+    const float horizontalLineX = x / 3.f;
     whole = std::abs(_startEndVertical.second - std::floor(_startEndVertical.second));
     float horizontalStartY = y / 13.5f + whole * step;
 
     // line size
-    sf::Vector2f verticalLineSize = sf::Vector2f(1, y - y / 13.5f);
-    sf::Vector2f verticalLineSizeBolded = sf::Vector2f(3, y - y / 13.5f);
-    sf::Vector2f horizontalLineSize = sf::Vector2f(graphWidth, 1);
-    sf::Vector2f horizontalLineSizeBolded = sf::Vector2f(graphWidth, 3);
+    const auto verticalLineSize = sf::Vector2f(1, y - y / 13.5f);
+    const auto verticalLineSizeBolded = sf::Vector2f(3, y - y / 13.5f);
+    const auto horizontalLineSize = sf::Vector2f(graphWidth, 1);
+    const auto horizontalLineSizeBolded = sf::Vector2f(graphWidth, 3);
 
     int a = 0;
     for (auto& i: _verticalLines) {
-        int zero = static_cast<int>(std::ceil(-_startEndHorizontal.first)) - 1;
+        const int zero = static_cast<int>(std::ceil(-_startEndHorizontal.first)) - 1;
         sf::Vector2f size = a == zero ? verticalLineSizeBolded : verticalLineSize;
         float posX = a == zero ? verticalStartX - 1 : verticalStartX;
         sf::Color color = a == zero ? sf::Color::Black : sf::Color(150, 150, 150);
@@ -61,7 +61,7 @@ void Canvas::set_lines(sf::RenderWindow& window) {
 
     int b = 0;
     for (auto& j: _horizontalLines) {
-        int zero = static_cast<int>(std::floor(_startEndVertical.second));
+        const int zero = static_cast<int>(std::floor(_startEndVertical.second));
         sf::Vector2f size = b == zero ? horizontalLineSizeBolded : horizontalLineSize;
         float posY = b == zero ? horizontalStartY - 1 : horizontalStartY;
         sf::Color color = b == zero ? sf::Color::Black : sf::Color(150, 150, 150);
@@ -79,13 +79,13 @@ std::vector<std::string>& Canvas::all_functions() {
 
 void Canvas::show_scale(sf::RenderWindow& window) {
     //windows size
-    float x = static_cast<float>(window.getSize().x);
-    float y = static_cast<float>(window.getSize().y);
+    const float x = static_cast<float>(window.getSize().x);
+    const float y = static_cast<float>(window.getSize().y);
 
     ImGui::SetNextWindowSize(ImVec2(x / 10.f, y / 22.f));
     ImGui::SetNextWindowPos(ImVec2(x - x / 6.f, y - y / 10.f));
 
-    int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    constexpr int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
 
     ImGui::Begin("Scale", nullptr, flags);
     ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[3]);
@@ -101,13 +101,13 @@ void Canvas::show_scale(sf::RenderWindow& window) {
 }
 
 void Canvas::show_numbers(sf::RenderWindow& window) const {
-    float x = static_cast<float>(window.getSize().x);
-    float y = static_cast<float>(window.getSize().y);
+    const float x = static_cast<float>(window.getSize().x);
+    const float y = static_cast<float>(window.getSize().y);
 
     // width of the coordinate system
-    float graphWidth = x - x / 3.f;
+    const float graphWidth = x - x / 3.f;
 
-    float step = graphWidth / 12.f;
+    const float step = graphWidth / 12.f;
 
     // vertical numbers
     vertical_numbers(x, graphWidth, step, y);
@@ -116,11 +116,11 @@ void Canvas::show_numbers(sf::RenderWindow& window) const {
     horizontal_numbers(x, step, y);
 }
 
-void Canvas::vertical_numbers(float x, float graphWidth, float step, float y) const {
-    float whole, fraction;
-    fraction = std::modf(_startEndVertical.second, &whole);
+void Canvas::vertical_numbers(const float x, const float graphWidth, const float step, const float y) const {
+    float whole;
+    const float fraction = std::modf(_startEndVertical.second, &whole);
     for (int i = 0; i < 11; ++i) {
-        auto j = static_cast<float>(i);
+        const auto j = static_cast<float>(i);
         float shift = std::abs(_startEndHorizontal.first) - 6.f;
 
         ImGui::SetNextWindowSize(ImVec2(0, 0));
@@ -133,13 +133,12 @@ void Canvas::vertical_numbers(float x, float graphWidth, float step, float y) co
         }
         ImGui::SetNextWindowPos(defaultNext);
 
-        int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize;
+        constexpr int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize;
 
         std::string title = "Vertical number" + std::to_string(i);
-        float number = whole / _scale - j / _scale;
-        if (number != 0) {
+        if (const float number = (whole - j) / _scale; number != 0) {
             std::string convertedNumber = std::to_string(number);
-            int afterComa = _scale > 1 ? 4 : 3;
+            const int afterComa = _scale > 1 ? 4 : 3;
             convertedNumber = convertedNumber.substr(0, convertedNumber.find('.') + afterComa);
 
             ImGui::Begin(title.c_str(), nullptr, flags);
@@ -152,10 +151,10 @@ void Canvas::vertical_numbers(float x, float graphWidth, float step, float y) co
 }
 
 void Canvas::horizontal_numbers(float x, float step, float y) const {
-    float whole, fraction;
-    fraction = std::modf(_startEndHorizontal.first, &whole);
+    float whole;
+    const float fraction = std::modf(_startEndHorizontal.first, &whole);
     for (int i = 0; i <= _verticalLines.size(); ++i) {
-        auto j = static_cast<float>(i);
+        const auto j = static_cast<float>(i);
         float shift = std::abs(_startEndVertical.second) - 5.f;
 
         ImGui::SetNextWindowSize(ImVec2(0, 0));
@@ -168,17 +167,17 @@ void Canvas::horizontal_numbers(float x, float step, float y) const {
         }
         ImGui::SetNextWindowPos(defaultNext);
 
-        int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize;
+        constexpr int flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize;
 
         std::string title = "Horizontal number" + std::to_string(i);
-        float number = whole / _scale + j / _scale;
+        const float number = (whole + j) / _scale;
 
         std::string convertedNumber;
         if (number == 0) {
             convertedNumber = "0";
         } else {
             convertedNumber = std::to_string(number);
-            int afterComa = _scale > 1 ? 4 : 3;
+            const int afterComa = _scale > 1 ? 4 : 3;
             convertedNumber = convertedNumber.substr(0, convertedNumber.find('.') + afterComa);
         }
 
@@ -202,12 +201,10 @@ void Canvas::scroll_scale(float delta) {
 std::vector<std::array<std::pair<float, float>, Canvas::pointNumber>> Canvas::evaluate_functions() {
     std::vector<std::array<std::pair<float, float>, pointNumber>> functions;
     for (std::string& func: _allFunctions) {
-        // az::Function function;
-        auto function = az::parse_expression(func);
-        // function.start(func);
+        auto const function = az::parse_expression(func);
         std::array<std::pair<float, float>, pointNumber> array;
         for (int i = 0; i < pointNumber; ++i) {
-            auto const j = static_cast<float>(i);
+            const auto j = static_cast<float>(i);
             float x = _startEndHorizontal.first / _scale + 12.f / static_cast<float>(pointNumber) / _scale * j;
             float y = function->evaluate(x);
             // try {
@@ -227,32 +224,28 @@ std::vector<sf::VertexArray> Canvas::prepare_graphs(sf::RenderWindow& window) {
 
     // calculating graph width
     auto width = static_cast<float>(window.getSize().x);
-    auto height = static_cast<float>(window.getSize().y);
+    const auto height = static_cast<float>(window.getSize().y);
     width = width - width / 3.f;
-    float step = width / 12.f;
+    const float step = width / 12.f;
 
     // scaling factor
-    float a = width / 12.f * _scale;
+    const float a = width / 12.f * _scale;
 
     // shifting coefficient
-    float b = width / 2.f;
+    const float b = width / 2.f;
 
     // vertical shift
-    float defaultZero = height / 13.5f + 5 * step;
-    float shift = step * (_startEndVertical.first + _startEndVertical.second) / 2;
-    float z = defaultZero + shift;
+    const float defaultZero = height / 13.5f + 5 * step;
+    const float shift = step * (_startEndVertical.first + _startEndVertical.second) / 2;
+    const float z = defaultZero + shift;
 
     auto functionsValues = evaluate_functions();
 
     int n = 0;
     for (const auto& i: functionsValues) {
         sf::VertexArray plot(sf::TriangleStrip);
-        for (auto point: i) {
-            if (!isnanf(point.second)) {
-                //get points
-                float x = point.first;
-                float y = point.second;
-
+        for (auto [x, y]: i) {
+            if (!isnanf(y)) {
                 x -= _startEndHorizontal.first / _scale;
 
                 // scale values
@@ -266,7 +259,7 @@ std::vector<sf::VertexArray> Canvas::prepare_graphs(sf::RenderWindow& window) {
                 sf::Vertex top, bottom;
                 auto color = _functionColors[n % 7];
 
-                float lineThickness = 2.f;
+                constexpr float lineThickness = 2.f;
 
                 top = sf::Vertex(sf::Vector2f(x, y + lineThickness), color);
                 bottom = sf::Vertex(sf::Vector2f(x, y - lineThickness), color);
@@ -284,14 +277,14 @@ std::vector<sf::VertexArray> Canvas::prepare_graphs(sf::RenderWindow& window) {
 
 void Canvas::shift(sf::RenderWindow& window, sf::Vector2i oldPosition, sf::Vector2i newPosition) {
     //get window size
-    auto size = window.getSize();
+    const auto size = window.getSize();
     //scaling factor to transform mouse change coordination to number between 0 and 12 for x-axis and 0 and 10 for y-axis
-    auto scale = static_cast<float>(size.y) - static_cast<float>(size.y) / 13.5f;
+    const auto scale = static_cast<float>(size.y) - static_cast<float>(size.y) / 13.5f;
 
-    auto changeOfPosition = newPosition - oldPosition;
+    const auto changeOfPosition = newPosition - oldPosition;
 
-    float xChange = static_cast<float>(changeOfPosition.x) / scale * 12;
-    float yChange = static_cast<float>(changeOfPosition.y) / scale * 10;
+    const float xChange = static_cast<float>(changeOfPosition.x) / scale * 12;
+    const float yChange = static_cast<float>(changeOfPosition.y) / scale * 10;
 
     _startEndHorizontal.first -= xChange;
     _startEndHorizontal.second -= xChange;
